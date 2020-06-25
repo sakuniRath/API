f85f6887ca8af752db0ef2c5f6ec9cd2e10f4f35
sakunim <sakuni.rathnayaka@gmail.com>
2020-06-25 17:56:01 +0530

    Add unit test class

--- ShoppingCartApp/Controllers/ProductListController.cs
+++ ShoppingCartApp/Controllers/ProductListController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ShoppingCartApp.BusinessLayer;
 using ShoppingCartApp.BusinessLayer.Models;
+using ShoppingCartApp.BusinessLayer.Services;
 
 namespace ShoppingCartApp.Controllers
 {
@@ -13,12 +15,23 @@ namespace ShoppingCartApp.Controllers
     public class ProductListController : ControllerBase
     {
         private ProductBL pbl = new ProductBL();
+        private readonly IProductService _service;
 
+        //[HttpGet]
+        //public async Task<IActionResult> Get()
+        //{
+
+        //    List<ProductModel> products = await pbl.GetAllProductList();
+        //    return Ok(products);
+        //}
+        public ProductListController(IProductService service)
+        {
+            _service = service;
+        }
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-
-            List<ProductModel> products = await pbl.GetAllProductList();
+            List<ProductModel> products = await _service.GetAllProductList();
             return Ok(products);
         }
     }
--- ShoppingCartApp/Startup.cs
+++ ShoppingCartApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ShoppingCartApp.BusinessLayer.Services;
 
 namespace ShoppingCartApp
 {
@@ -27,6 +28,7 @@ namespace ShoppingCartApp
         {
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
